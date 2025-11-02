1cc961112c6133a7e3f861cfd8b770f7917a09fe
B1ZON-c0de <roman.andreicko@icloud.com>
2025-11-03 04:40:35 +0700

    init footer component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import LayoutContainer from './components/LayoutContainer.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
-  <div class="flex min-h-screen w-full flex-col bg-gray-100">
-    <header class="fixed top-0 right-0 left-0 bg-white">
+  <div
+    class="flex min-h-screen w-full flex-col bg-gray-100"
+  >
+    <header
+      class="fixed top-0 right-0 left-0 bg-white shadow-md"
+    >
       <LayoutHeader />
     </header>
     <main class="mt-26 flex-1"><RouterView /></main>
     <footer class="bg-white">
-      <LayoutContainer>Подвал</LayoutContainer>
+      <LayoutFooter />
     </footer>
   </div>
 </template>
