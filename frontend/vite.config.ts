7b3f347e90c36d3ac51fdb986a94f3d493c2235b
B1ZON-c0de <roman.andreicko@icloud.com>
2025-11-09 20:26:05 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ const backendUrl = 'http://localhost:3002'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5174,
+    port: 3002,
     proxy: {
       '/api': {
         target: backendUrl,
